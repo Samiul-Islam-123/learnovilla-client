f4ba09ab98924f862eb3965505aaf464bb6ecee0
SourishSamanta <sourishsamanta1234@gmail.com>
2024-04-05 05:28:50 +0530

    dashboard initiated

--- src/RouteControllers/MainRoutes.jsx
+++ src/RouteControllers/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from '../Pages/LandingPage'
 import Login from '../Pages/AuthPages/Login'
 import SignUp from '../Pages/AuthPages/Signup'
+import Dashboard from '../Pages/Dashboard'
 
 function MainRoutes() {
   return (
@@ -11,6 +12,7 @@ function MainRoutes() {
             <Route exact path='/' element={<LandingPage />}></Route>
             <Route exact path='/login' element={<Login />}></Route>
             <Route exact path='/signup' element={<SignUp />}></Route>
+            <Route exact path='/dashboard' element={<Dashboard />}></Route>
 
         </Routes>
     </>
